591f920233142d3b90117fbdb9087053206dcecb
LRYDark <reinert.joris@gmail.com>
2024-11-07 18:15:06 +0100

    V1

---
+++ hook.php
@@ -0,0 +1,64 @@
+<?php
+
+/*if (isset($_POST['save_selection'])) {
+   PluginGestionTicket::processSelection();  
+}*/
+
+function plugin_gestion_install() { // fonction installation du plugin
+   
+   $rep_files_rp = GLPI_PLUGIN_DOC_DIR . "/gestion";
+   if (!is_dir($rep_files_rp))
+      mkdir($rep_files_rp);
+
+   $rep_files_rp = GLPI_PLUGIN_DOC_DIR . "/gestion/signed";
+   if (!is_dir($rep_files_rp))
+      mkdir($rep_files_rp);
+
+   $rep_files_rp = GLPI_PLUGIN_DOC_DIR . "/gestion/unsigned";
+   if (!is_dir($rep_files_rp))
+      mkdir($rep_files_rp);
+
+   $migration = new Migration(PLUGIN_GESTION_VERSION);
+
+   // Parse inc directory
+   foreach (glob(dirname(__FILE__).'/inc/*') as $filepath) {
+      // Load *.class.php files and get the class name
+      if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+         $classname = 'PluginGestion' . ucfirst($matches[1]);
+         include_once($filepath);
+         // If the install method exists, load it
+         if (method_exists($classname, 'install')) {
+            $classname::install($migration);
+         }
+      }
+   }
+   $migration->executeMigration();
+   return true;
+}
+
+function plugin_gestion_uninstall() { // fonction desintallation du plugin
+
+   $rep_files_rp = GLPI_PLUGIN_DOC_DIR . "/gestion";
+   Toolbox::deleteDir($rep_files_rp);
+
+   $migration = new Migration(PLUGIN_GESTION_VERSION);
+
+   // Parse inc directory
+   foreach (glob(dirname(__FILE__).'/inc/*') as $filepath) {
+      // Load *.class.php files and get the class name
+      if (preg_match("/inc.(.+)\.class.php/", $filepath, $matches)) {
+         $classname = 'PluginGestion' . ucfirst($matches[1]);
+         include_once($filepath);
+         // If the install method exists, load it
+         if (method_exists($classname, 'uninstall')) {
+            $classname::uninstall($migration);
+         }
+      }
+   }
+
+   $migration->executeMigration();
+   return true;
+}
+
+
+
---
+++ setup.php
@@ -0,0 +1,62 @@
+<?php
+define('PLUGIN_GESTION_VERSION', '1.0.0'); // version du plugin
+
+// Minimal GLPI version,
+define("PLUGIN_GESTION_MIN_GLPI", "10.0.3");
+// Maximum GLPI version,
+define("PLUGIN_GESTION_MAX_GLPI", "10.2.0");
+
+define("PLUGIN_GESTION_WEBDIR", Plugin::getWebDir("gestion"));
+define("PLUGIN_GESTION_DIR", Plugin::getPhpDir("gestion"));
+define("PLUGIN_GESTION_NOTFULL_DIR", Plugin::getPhpDir("gestion",false));
+define("PLUGIN_GESTION_NOTFULL_WEBDIR", Plugin::getWebDir("gestion",false));
+
+function plugin_init_gestion() { // fonction glpi d'initialisation du plugin
+   global $PLUGIN_HOOKS, $CFG_GLPI;
+
+   $PLUGIN_HOOKS['csrf_compliant']['gestion'] = true;
+   $PLUGIN_HOOKS['change_profile']['gestion'] = ['PluginGestionProfile', 'initProfile'];
+
+   $plugin = new Plugin();
+
+   if (Session::getLoginUserID()) {
+      Plugin::registerClass('PluginGestionProfile', ['addtabon' => 'Profile']);
+   }
+
+   if (isset($_SESSION['glpiactiveprofile']['interface'])
+      && $_SESSION['glpiactiveprofile']['interface'] == 'central') {
+      $PLUGIN_HOOKS['add_javascript']['gestion'] = ['scripts/scripts-gestion.js'];
+   }
+
+   if ($plugin->isActivated('gestion')){ // verification si le plugin gestion est installé et activé
+      Plugin::registerClass('PluginGestionTicket', ['addtabon' => 'Ticket']);
+
+      $PLUGIN_HOOKS['config_page']['gestion'] = 'front/config.form.php'; // initialisation de la page config
+      Plugin::registerClass('PluginGestionConfig', ['addtabon' => 'Config']); // ajout de la de la class config dans glpi
+
+      $PLUGIN_HOOKS['post_show_item']['gestion'] = ['PluginGestionTicket', 'postShowItemNewTicketGESTION']; // initialisation de la class
+      $PLUGIN_HOOKS['pre_show_item']['gestion'] = ['PluginGestionTicket', 'postShowItemNewTaskGESTION']; // initialisation de la class
+   }
+}
+
+function plugin_version_gestion() { // fonction version du plugin (verification et affichage des infos de la version)
+   return [
+      'name'           => _n('Gestion', 'Gestion', 2, 'gestion'),
+      'version'        => PLUGIN_GESTION_VERSION,
+      'author'         => 'REINERT Joris',
+      'homepage'       => 'https://www.jcd-groupe.fr',
+      'requirements'   => [
+         'glpi' => [
+            'min' => PLUGIN_GESTION_MIN_GLPI,
+            'max' => PLUGIN_GESTION_MAX_GLPI,
+         ]
+      ]
+   ];
+}
+
+/**
+ * @return bool
+ */
+function plugin_gestion_check_prerequisites() {
+   return true;
+}
