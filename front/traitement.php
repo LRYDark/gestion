5306722d1335d2c4881e67dee5b2a193268966f0
LRYDark <reinert.joris@gmail.com>
2025-11-03 23:30:43 +0100

    1.5.5

--- front/traitement.php
+++ front/traitement.php
@@ -362,13 +362,14 @@ if ($pdf->Output('F', $outputPathTemp) === '') {
     }
 
     if (!empty($config->fields['CounterInvoice']) && (int)$config->fields['CounterInvoice'] === 1 && !empty($_POST['CounterInvoiceClient']) && (int)$_POST['CounterInvoiceClient'] === 1) {
-        if (!empty($config->fields['CounterInvoiceMail'])){  
+        if (!empty($config->fields['CounterInvoiceMail'])){              
+            if (empty($DOC->doc_id)) {$DOC->doc_id = "Aucun ticket lié";}
 
             if (!empty($_POST['relatedInvoiceToBL'])){
                 $relatedInvoiceToBL = $_POST['relatedInvoiceToBL'];
-                $ValueForSigned = "Bon de Livraison signé et règlement effectué au comptoir : $DOC_NAME <br><br> Documents/Informations associé au bon de livraison : $relatedInvoiceToBL <br><br> Mail client : $EMAIL";
+                $ValueForSigned = "Bon de Livraison signé et règlement effectué au comptoir : $DOC_NAME <br><br> Documents/Informations associé au bon de livraison : $relatedInvoiceToBL <br><br> Mail client : $EMAIL <br><br> Ticket ID : $DOC->doc_id";
             }else{
-                $ValueForSigned = "Bon de Livraison signé et règlement effectué au comptoir : $DOC_NAME <br><br> Mail client : $EMAIL";
+                $ValueForSigned = "Bon de Livraison signé et règlement effectué au comptoir : $DOC_NAME <br><br> Mail client : $EMAIL <br><br> Ticket ID : $DOC->doc_id";
             }
                 if (!empty($config->fields['ZenDocMail'])){ 
                     $sharepoint->MailSend($config->fields['ZenDocMail'].','.$config->fields['CounterInvoiceMail'], 0, $outputPathTemp, " ", $id_survey = NULL, $tracker = NULL, $webUrl = NULL, $fileName = NULL, "Bon de Livraison signé + règlement comptoir ", $ValueForSigned);
